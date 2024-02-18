e6ae370c81dd91a8190231adf51bfa493678fdc9
ParallaX12 <saalim.araf@gmail.com>
2024-02-18 17:48:52 +0600

    add bus ticket card

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 function offerButton() {
-    var button = document.getElementById('offerButton');
+    var button = document.getElementById("offerButton");
     button.innerHTML = 'No more offers at the moment';
-    setTimeout(function() {
-      button.innerHTML = 'Click me';
+    setTimeout(function () {
+      button.innerHTML = 'See all Offers';
     }, 5000);
   }
+
+// add a smooth scroll while taking the user to the purchase tickets section
+function smoothScroll(className) {
+    const elements = document.querySelectorAll(className);
+    elements.forEach(element => {
+      element.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = "#purchase-tickets";
+        document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
+      });
+    });
+  }
+  
+
+smoothScroll('.buy-tickets');
+
